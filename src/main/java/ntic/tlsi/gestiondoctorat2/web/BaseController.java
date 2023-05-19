d41e874c78e763b81e123e6793c1c9b801d94b11
Mossab dehane <mossabdehane19@gmail.com>
2023-05-19 13:35:52 +0200

    adding CandidatPage.html interface for candidat users

--- src/main/java/ntic/tlsi/gestiondoctorat2/web/BaseController.java
+++ src/main/java/ntic/tlsi/gestiondoctorat2/web/BaseController.java
@@ -1,10 +1,27 @@
 package ntic.tlsi.gestiondoctorat2.web;
 
+import lombok.AllArgsConstructor;
+import ntic.tlsi.gestiondoctorat2.entities.Candidat;
+import ntic.tlsi.gestiondoctorat2.entities.DTO.CandidatDTO;
+import ntic.tlsi.gestiondoctorat2.entities.User;
+import ntic.tlsi.gestiondoctorat2.repo.CandidatRepo;
+import ntic.tlsi.gestiondoctorat2.sec.UserDetailServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.Optional;
+
 @Controller
+
 public class BaseController {
+
+    @Autowired
+    private CandidatRepo candidatRepo;
+
     @GetMapping("/")
     public String homePage() {
         return "HomePage"; // index.html is your home page template file
@@ -14,6 +31,15 @@ public class BaseController {
     public String AdminPage(){
         return"AdminPage";
     }
+
+    @GetMapping("/candidatPage")
+    public String CandidatPage( Authentication authentication, Model model){
+        String username = authentication.getName();
+        Optional<User> candidat = candidatRepo.findByUsername(username);
+        model.addAttribute("candidat", candidat);
+        return"CandidatPage";}
+
+    }
 /*
     @GetMapping("/loginPage")
     public String login(){
@@ -21,4 +47,4 @@ public class BaseController {
     }*/
 
 
-}
+
